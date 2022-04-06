47c0cfccaa3f31985c93dc0d41a32884adc19629
Florian Simmer <Floriansimmer@users.noreply.github.com>
2022-04-06 19:57:44 +0200

    black format

--- components/amis/sensor.py
+++ components/amis/sensor.py
@@ -20,33 +20,49 @@ DEPENDENCIES = ["uart"]
 amis_ns = cg.esphome_ns.namespace("amis")
 AMISComponent = amis_ns.class_("AMISComponent", cg.Component, uart.UARTDevice)
 
-CONF_POWER_GRID_KEY = 'power_grid_key'
-CONF_ENERGY_A_POSITIVE = 'energy_a_positive'
-CONF_ENERGY_A_NEGATIVE = 'energy_a_negative'
-CONF_REACTIVE_ENERGY_A_POSITIVE = 'reactive_energy_a_positive'
-CONF_REACTIVE_ENERGY_A_NEGATIVE = 'reactive_energy_a_negative'
-CONF_INSTANTANEOUS_POWER_A_POSITIVE = 'instantaneous_power_a_positive'
-CONF_INSTANTANEOUS_POWER_A_NEGATIVE = 'instantaneous_power_a_negative'
-CONF_REACTIVE_INSTANTANEOUS_POWER_A_POSITIVE = 'reactive_instantaneous_power_a_positive'
-CONF_REACTIVE_INSTANTANEOUS_POWER_A_NEGATIVE = 'reactive_instantaneous_power_a_negative'
-CONF_TIMESTAMP = 'timestamp'
+CONF_POWER_GRID_KEY = "power_grid_key"
+CONF_ENERGY_A_POSITIVE = "energy_a_positive"
+CONF_ENERGY_A_NEGATIVE = "energy_a_negative"
+CONF_REACTIVE_ENERGY_A_POSITIVE = "reactive_energy_a_positive"
+CONF_REACTIVE_ENERGY_A_NEGATIVE = "reactive_energy_a_negative"
+CONF_INSTANTANEOUS_POWER_A_POSITIVE = "instantaneous_power_a_positive"
+CONF_INSTANTANEOUS_POWER_A_NEGATIVE = "instantaneous_power_a_negative"
+CONF_REACTIVE_INSTANTANEOUS_POWER_A_POSITIVE = "reactive_instantaneous_power_a_positive"
+CONF_REACTIVE_INSTANTANEOUS_POWER_A_NEGATIVE = "reactive_instantaneous_power_a_negative"
+CONF_TIMESTAMP = "timestamp"
 
 CONFIG_SCHEMA = (
     cv.Schema(
         {
-            cv.Required(CONF_POWER_GRID_KEY): cv.string, 
+            cv.Required(CONF_POWER_GRID_KEY): cv.string,
             cv.GenerateID(): cv.declare_id(AMISComponent),
             cv.Optional(CONF_ENERGY_A_POSITIVE): sensor.sensor_schema(
-                UNIT_WATT_HOURS, ICON_EMPTY, 0, DEVICE_CLASS_ENERGY, STATE_CLASS_MEASUREMENT
+                UNIT_WATT_HOURS,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_ENERGY,
+                STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_ENERGY_A_NEGATIVE): sensor.sensor_schema(
-                UNIT_WATT_HOURS, ICON_EMPTY, 0, DEVICE_CLASS_ENERGY, STATE_CLASS_MEASUREMENT
+                UNIT_WATT_HOURS,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_ENERGY,
+                STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_REACTIVE_ENERGY_A_POSITIVE): sensor.sensor_schema(
-                UNIT_VOLT_AMPS_REACTIVE_HOURS, ICON_EMPTY, 0, DEVICE_CLASS_ENERGY, STATE_CLASS_MEASUREMENT
+                UNIT_VOLT_AMPS_REACTIVE_HOURS,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_ENERGY,
+                STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_REACTIVE_ENERGY_A_NEGATIVE): sensor.sensor_schema(
-                UNIT_VOLT_AMPS_REACTIVE_HOURS, ICON_EMPTY, 0, DEVICE_CLASS_ENERGY, STATE_CLASS_MEASUREMENT
+                UNIT_VOLT_AMPS_REACTIVE_HOURS,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_ENERGY,
+                STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_INSTANTANEOUS_POWER_A_POSITIVE): sensor.sensor_schema(
                 UNIT_WATT, ICON_EMPTY, 0, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
@@ -54,15 +70,27 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_INSTANTANEOUS_POWER_A_NEGATIVE): sensor.sensor_schema(
                 UNIT_WATT, ICON_EMPTY, 0, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
             ),
-            cv.Optional(CONF_REACTIVE_INSTANTANEOUS_POWER_A_POSITIVE): sensor.sensor_schema(
-                UNIT_VOLT_AMPS_REACTIVE, ICON_EMPTY, 0, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            cv.Optional(
+                CONF_REACTIVE_INSTANTANEOUS_POWER_A_POSITIVE
+            ): sensor.sensor_schema(
+                UNIT_VOLT_AMPS_REACTIVE,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_POWER,
+                STATE_CLASS_MEASUREMENT,
             ),
-            cv.Optional(CONF_REACTIVE_INSTANTANEOUS_POWER_A_NEGATIVE): sensor.sensor_schema(
-                UNIT_VOLT_AMPS_REACTIVE, ICON_EMPTY, 0, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            cv.Optional(
+                CONF_REACTIVE_INSTANTANEOUS_POWER_A_NEGATIVE
+            ): sensor.sensor_schema(
+                UNIT_VOLT_AMPS_REACTIVE,
+                ICON_EMPTY,
+                0,
+                DEVICE_CLASS_POWER,
+                STATE_CLASS_MEASUREMENT,
             ),
-#            cv.Optional(CONF_TIMESTAMP): sensor.sensor_schema(
-#                UNIT_SECOND, ICON_EMPTY, 0, DEVICE_CLASS_TIMESTAMP
-#            ),
+            #            cv.Optional(CONF_TIMESTAMP): sensor.sensor_schema(
+            #                UNIT_SECOND, ICON_EMPTY, 0, DEVICE_CLASS_TIMESTAMP
+            #            ),
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
@@ -74,9 +102,9 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
-    
+
     cg.add(var.set_power_grid_key(config[CONF_POWER_GRID_KEY]))
-    
+
     for key in [
         CONF_ENERGY_A_POSITIVE,
         CONF_ENERGY_A_NEGATIVE,
